001b47455602d8f8b46fed2dda262d2bf6156e53
Alice Gonçalves <galice9992@gmail.com>
2022-02-17 02:13:45 -0300

    atualizando a mensagem quando nenhum campo é passado para atualizar uma tarefa

--- src/middlewares/tasks/authUpdateBody.js
+++ src/middlewares/tasks/authUpdateBody.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
     const { name, description, status} = req.body
     if (!name && !description && !status) {
         return res.status(StatusCodes.BAD_REQUEST)
-          .json({ message: "must have at least 1 property on body to update"})
+          .json({ message: "Must have at least 1 property on body to update"})
     }
     const { error } = taskUpdateSchema.validate({ name, description, status });
     if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message })
